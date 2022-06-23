4645db73c78d1af3804496f0567b6a3f84309804
vtnboolean <vnthuanx@gmail.com>
2022-06-23 16:49:17 +0200

    done without bonus

--- js/main.js
+++ js/main.js
@@ -1,17 +1,30 @@
 // recupero il parent
 const gridContainer = document.getElementById('grid-container');
 
-// ciclo per creare i quadratini
-for (let i=0; i < 100; i++){
-    const newSquare = createNewSquare();
-    newSquare.addEventListener('click', function(){
-        newSquare.classList.toggle('azure');
-    })
-}
+const button = document.querySelector('#button')
+button.addEventListener('click', function(){
+    // ciclo per creare i quadratini
+    for (let i=1; i < 101; i++){
+        const newSquare = createNewSquare();
+        gridContainer.append(newSquare);
+
+        newSquare.addEventListener('click', function(){
+            newSquare.classList.toggle('azure');
+            console.log(`hai cliccato sulla cella ${i}`);
+            
+        })
+    }
+    
+})
+
+
+
+
 
 // funzione per creare i quadratini
 function createNewSquare(){
     const currentSquare = document.createElement('div');
-    currentSquare.classList.add('div.square');
+    currentSquare.classList.add('square');
     return currentSquare;
 }
+
