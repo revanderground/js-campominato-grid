56a45a673d2c4c1605abd711e278c28b4535f1d4
vtnboolean <vnthuanx@gmail.com>
2022-06-23 17:25:37 +0200

    with bonus

--- js/main.js
+++ js/main.js
@@ -3,19 +3,47 @@ const gridContainer = document.getElementById('grid-container');
 
 const button = document.querySelector('#button')
 button.addEventListener('click', function(){
-    // ciclo per creare i quadratini
-    for (let i=1; i < 101; i++){
-        const newSquare = createNewSquare();
-        gridContainer.append(newSquare);
+    
+    const userChoice = document.getElementById('input-difficulty').value;
+    if (userChoice == "level-1"){
+           // ciclo per creare i quadratini
+        for (let i=1; i < 101; i++){
+            const newSquare = createNewSquare();
+            gridContainer.append(newSquare);
 
-        newSquare.addEventListener('click', function(){
-            newSquare.classList.toggle('azure');
-            console.log(`hai cliccato sulla cella ${i}`);
-            
-        })
+            newSquare.addEventListener('click', function(){
+                newSquare.classList.toggle('azure');
+                console.log(`hai cliccato sulla cella ${i}`);
+                
+            })
+        }
+    } else if (userChoice == "level-2"){
+        // ciclo per creare i quadratini
+     for (let i=1; i < 82; i++){
+         const newSquare = createNewSquare2();
+         gridContainer.append(newSquare);
+
+         newSquare.addEventListener('click', function(){
+             newSquare.classList.toggle('azure');
+             console.log(`hai cliccato sulla cella ${i}`);
+             
+         })
+     }
+    } else if (userChoice == "level-3"){
+        for (let i=1; i < 50; i++){
+            const newSquare = createNewSquare3();
+            gridContainer.append(newSquare);
+   
+            newSquare.addEventListener('click', function(){
+                newSquare.classList.toggle('azure');
+                console.log(`hai cliccato sulla cella ${i}`);
+                
+            })
     }
     
-})
+ 
+    
+}})
 
 
 
@@ -28,3 +56,16 @@ function createNewSquare(){
     return currentSquare;
 }
 
+// funzione per creare i quadratini
+function createNewSquare2(){
+    const currentSquare = document.createElement('div');
+    currentSquare.classList.add('square2');
+    return currentSquare;
+}
+
+// funzione per creare i quadratini
+function createNewSquare3(){
+    const currentSquare = document.createElement('div');
+    currentSquare.classList.add('square3');
+    return currentSquare;
+}
